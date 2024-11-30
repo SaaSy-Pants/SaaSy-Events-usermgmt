2eb53e347d9228cdfcaa929a4ffc15c8d88a0e9b
Abhishek Paul <paul.abhishek2000@gmail.com>
2024-11-30 09:52:33 -0500

    modified user endpoints to handle jwt in auth header

--- app/models/organiser.py
+++ app/models/organiser.py
@@ -8,7 +8,6 @@ class Organiser(BaseModel):
     Name: str
     Email: EmailStr = constr(min_length=1, max_length=50)
     PhoneNo: str = constr(min_length=1, max_length=15)
-    HashedPswd: str = constr(min_length=1, max_length=255)
     Address: str
     Age: int
 
@@ -19,7 +18,6 @@ class Organiser(BaseModel):
                 "Name": "Alice Johnson",
                 "Email": "alice.johnson@example.com",
                 "PhoneNo": "+1987654321",
-                "HashedPswd": "$2b$12$W3Nm.HN38Dmhke6GfEvZneiNUM3pjbl6a7D2SGnQXWkhnMIYsn.H6",
                 "Address": "789 Maple Ave, Cityville, Country",
                 "Age": "28"
             }
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,9 @@
-from typing import Annotated
-
-from fastapi import APIRouter
-from fastapi.params import Form
+from fastapi import APIRouter, Request
 from starlette.responses import JSONResponse
 
 from app.models.user import User
 from app.resources import user_resource
-from app.utils.utils import hash_password, authenticate_profile
+from app.utils.utils import extract_access_token_from_header, verify_custom_jwt
 
 user_router = APIRouter()
 
@@ -17,8 +14,13 @@ user_router = APIRouter()
         500: {"description": "Database not live"},
     }
 )
-async def create_user(user: User):
-    user.HashedPswd = hash_password(user.HashedPswd)
+async def create_user(user: User, request: Request):
+    access_token = extract_access_token_from_header(request)
+    user_info = verify_custom_jwt(access_token, profile='user')
+
+    if not user_info or user_info.get('email') != user.Email:
+        return JSONResponse(content={'error': 'Access denied'}, status_code=403)
+
     resource = user_resource.UserResource(config = None)
     result = resource.insert_data(user)
     if result['error'] is not None:
@@ -30,16 +32,19 @@ async def create_user(user: User):
         result['UID'] = user.UID
         return JSONResponse(content=result, status_code=201)
 
-@user_router.get(path="/{userId}", tags=["users"],
+@user_router.get(path="", tags=["users"],
     responses={
         200: {"description": "User fetched successfully"},
-        404: {"description": "Id does not exist"},
+        404: {"description": "User does not exist"},
         500: {"description": "Database not live"},
     }
 )
-async def get_user(userId: str):
+async def get_user(request: Request):
+    access_token = extract_access_token_from_header(request)
+    user_info = verify_custom_jwt(access_token, profile='user')
+
     resource = user_resource.UserResource(config = None)
-    result = resource.get_by_key(userId)
+    result = resource.get_by_custom_key('Email', user_info['email'])
 
     if result['error'] is not None:
         if result['status'] == 'bad request':
@@ -50,38 +55,6 @@ async def get_user(userId: str):
         return JSONResponse(content=result, status_code=200)
 
 
-'''
-Below are the actual credentials for users. The passwords stored in DB are hashed.
-
-johndoe@example.com, aaaaaaaa
-janesmith@example.com, bbbbbbbb
-robertbrown@example.com, cccccccc
-emilydavis@example.com, dddddddd
-michaeljohnson@example.com, eeeeeeee
-'''
-@user_router.post("/authenticate", tags=["users"],
-    responses={
-        200: {"description": "Authentication Successful"},
-        401: {"description": "Unauthenticated - Incorrect Password"},
-        404: {"description": "User not found"},
-    }
-)
-async def authenticate(email: Annotated[str, Form()], password: Annotated[str, Form()]):
-    resource = user_resource.UserResource(config = None)
-    result = resource.get_by_custom_key("Email", email)
-
-    if result['error'] is not None:
-        if result['status'] == 'bad request':
-            return JSONResponse(content={"message": f"{email} not found!"}, status_code=404)
-        else:
-            return JSONResponse(content=result, status_code=500)
-    else:
-        print(result)
-        if authenticate_profile(password, result['details']):
-            return JSONResponse(content={"message": f"Authentication Successful!, User: {result['details']['Name']}"},status_code=200)
-        else:
-            return JSONResponse(content={"message": f"Authentication - Unsuccessful! Incorrect Password for {email}"},status_code=401)
-
 @user_router.put(path="", tags=["users"],
     responses={
         200: {"description": "User modification successful"},
@@ -89,8 +62,13 @@ async def authenticate(email: Annotated[str, Form()], password: Annotated[str, F
         500: {"description": "Database not live"}
     }
 )
-async def modify_user(user: User):
-    user.HashedPswd = hash_password(user.HashedPswd)
+async def modify_user(user: User, request: Request):
+    access_token = extract_access_token_from_header(request)
+    user_info = verify_custom_jwt(access_token, profile='user')
+
+    if not user_info or user_info.get('email') != user.Email:
+        return JSONResponse(content={'error': 'Access denied'}, status_code=403)
+
     resource = user_resource.UserResource(config = None)
     result = resource.modify_data(user)
     if result['error'] is not None:
@@ -103,16 +81,20 @@ async def modify_user(user: User):
         return JSONResponse(content=result, status_code=200)
 
 
-@user_router.delete(path="/{userId}", tags=["users"],
+@user_router.delete(path="", tags=["users"],
     responses={
         204: {"description": "User deletion successful"},
         404: {"description": "User not found"},
         500: {"description": "Database not live"}
     }
 )
-async def delete_user(userId: str):
+async def delete_user(request: Request):
+    access_token = extract_access_token_from_header(request)
+    user_info = verify_custom_jwt(access_token, profile='user')
+
     resource = user_resource.UserResource(config = None)
-    result = resource.delete_data_by_key(userId)
+    result = resource.delete_data_by_custom_key('Email', user_info['email'])
+
     if result['error'] is not None:
         if result['status'] == 'bad request':
             return JSONResponse(content=result, status_code=404)
